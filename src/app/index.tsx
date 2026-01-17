82e6ad1c840bc83637ff625f4b4efb040da55003
Guilherme Augusto <guilhermedev23@gmail.com>
2026-01-17 01:42:55 -0300

    ok

---
+++ src/app/index.tsx
@@ -0,0 +1,21 @@
+import { fontFamily } from '@/theme/fontFamily'
+import { router } from 'expo-router'
+import { Button, Text, View } from 'react-native'
+
+export default function Index() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center' }}>
+      <Text style={{ fontFamily:fontFamily.bold , fontSize:34 }}>Expo Router</Text>
+
+      <Button title="Nova meta" onPress={() => router.navigate('/target')} />
+      <Button
+        title="Transação"
+        onPress={() => router.navigate('/transaction/765890')}
+      />
+      <Button
+        title="Progresso"
+        onPress={() => router.navigate('/in-progress/12')}
+      />
+    </View>
+  )
+}
