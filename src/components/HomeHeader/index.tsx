81b8ef49e33ec8d2716a368c87f6a83266e6350d
Guilherme Augusto <guilhermedev23@gmail.com>
2026-01-19 19:25:52 -0300

    ok

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,10 +1,25 @@
 import { LinearGradient } from "expo-linear-gradient";
 import { styles } from "./styles";
 import { colors } from "@/theme/colors";
+import { View,Text } from "react-native";
+
+
+export type HomeHeaderProps ={
+    total: string
+}
+
+type Props = {
+    total:string
+}
+
 export function HomeHeader(){
     return(
-        <LinearGradient colors={[colors.blue[500],colors.blue[800]]} style={styles.container}>
-            
+        <LinearGradient colors={[colors.blue[500],colors.blue[800]]}
+         style={styles.container}
+         >
+        <View>
+         <Text style={styles.label}>Total que você possui</Text>
+        </View>
         </LinearGradient>
     )
 }
