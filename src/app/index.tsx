7c4dd8b0b158e710c37ff7a8fca3a69c85603b97
Guilherme Augusto <guilhermedev23@gmail.com>
2026-01-26 16:18:48 -0300

    ok

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,26 +1,55 @@
-import { View } from 'react-native'
-import { HomeHeader } from "@/components/HomeHeader"
+import { Button } from '@/components/Button'
+import { HomeHeader } from '@/components/HomeHeader'
+import { List } from '@/components/List'
 import { Target } from '@/components/Target'
+import { View } from 'react-native'
 const summary = {
-  total: "R$ 2.680,00",
-  input: { label: "Entradas", value: "R$ 6.184.90" },
-  output: { label: "Saídas", value: "-R$ 883.66" },
+  total: 'R$ 2.680,00',
+  input: { label: 'Entradas', value: 'R$ 6.184,90' },
+  output: { label: 'Saídas', value: '-R$ 883,65' },
 }
-
 
 const targets = [
   {
-    name: "Comprar uma cadeira ergonômica",
-    current: "900,00",
-    percentage: "75%",
-    target: "1.200,00",
-  }
+    id: '1',
+    name: 'Apple Watch',
+    percentage: '50%',
+    current: 'R$ 580,00',
+    target: 'R$ 1.790,00',
+  },
+  {
+    id: '2',
+    name: 'Comprar uma cadeira ergonômica',
+    percentage: '75%',
+    current: 'R$ 900,00',
+    target: 'R$ 1.200,00',
+  },
+  {
+    id: '3',
+    name: 'Comprar uma cadeira ergonômica',
+    percentage: '75%',
+    current: 'R$ 1.200,00',
+    target: 'R$ 3.000,00',
+  },
 ]
+
 export default function Index() {
   return (
-    <View style={{ flex: 1, }}>
+    <View style={{ flex: 1 }}>
       <HomeHeader data={summary} />
-      <Target data={targets[0]}  />
+
+      <List
+        title="Metas"
+        data={[]}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Target data={item} />}
+        emptyMessage="Nenhuma meta. Toque em nova meta para criar."
+        containerStyle={{ paddingHorizontal: 24 }}
+      />
+
+       <View style={{ padding:24, paddingBottom:32}}>
+        <Button title="Nova Meta" />
+       </View>
     </View>
   )
 }
